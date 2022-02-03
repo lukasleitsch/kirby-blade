33a4d7eeee46e25f3b544c5e9a460f6a1797e5b5
Fabian Michael <hallo@fabianmichael.de>
2022-02-03 09:19:07 +0100

    User composer autoloading for helpers.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ use Afbora\Template;
 use Kirby\Cms\App as Kirby;
 
 @include_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/helpers.php';
 
 Kirby::plugin('afbora/blade', [
     'options' => [
