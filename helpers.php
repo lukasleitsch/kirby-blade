76f03e0ab396e4edea11a4a48f22bd5828a992cc
Lukas Leitsch <lukas@leitsch.org>
2022-02-03 13:35:50 +0100

    fix code style

--- helpers.php
+++ helpers.php
@@ -5,19 +5,19 @@ use Illuminate\Support\Str;
 use Illuminate\View\ComponentAttributeBag;
 
 /**
-* Render a blade component.
+ * Render a blade component.
  * @param string $name Component name, exclude component folder (i.e use "card" instead of "components.card")
  * @param array $props Component properties
  * @param array $attributes Component attributes
  * @return \Illuminate\Contracts\View\View
  */
-if (!function_exists('component')) {
-    function component($name, $props = [], $attributes = []) {
-        $className = collect(explode('.', $name))->map(function($part) {
-            return Str::studly($part);
-        })->join('\\');
+if (! function_exists('component')) {
+    function component($name, $props = [], $attributes = [])
+    {
+        $className = collect(explode('.', $name))->map(fn($part) => Str::studly($part))->join('\\');
         $className = "App\\View\\Components\\{$className}";
-        if(class_exists($className)) {
+
+        if (class_exists($className)) {
             $reflection = (new \ReflectionClass($className))->getConstructor();
             $parameters = [];
             foreach ($reflection->getParameters() as $param) {
@@ -25,10 +25,12 @@ if (!function_exists('component')) {
             }
             $component = new $className(...$parameters);
             $component->withAttributes($attributes);
+
             return $component->render()->with($component->data());
         }
 
         $props['attributes'] = new ComponentAttributeBag($attributes);
+
         return View::make("components.{$name}", $props);
     }
 }
