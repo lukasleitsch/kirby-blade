48d571df9e1826f6e4c9cc65b6c934a1e553974a
Fabian Michael <hallo@fabianmichael.de>
2022-02-03 09:23:08 +0100

    Add function_exists() check

--- helpers.php
+++ helpers.php
@@ -2,30 +2,33 @@
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Str;
+use Illuminate\View\ComponentAttributeBag;
 
 /**
- * Render a blade component.
+* Render a blade component.
  * @param string $name Component name, exclude component folder (i.e use "card" instead of "components.card")
  * @param array $props Component properties
  * @param array $attributes Component attributes
  * @return \Illuminate\Contracts\View\View
  */
-function component($name, $props = [], $attributes = []) {
-    $className = collect(explode('.', $name))->map(function($part) {
-        return Str::studly($part);
-    })->join('\\');
-    $className = "App\\View\\Components\\{$className}";
-    if(class_exists($className)) {
-        $reflection = (new \ReflectionClass($className))->getConstructor();
-        $parameters = [];
-        foreach ($reflection->getParameters() as $param) {
-            $parameters[] = $props[$param->name] ?? $param->getDefaultValue();
+if (!function_exists('component')) {
+    function component($name, $props = [], $attributes = []) {
+        $className = collect(explode('.', $name))->map(function($part) {
+            return Str::studly($part);
+        })->join('\\');
+        $className = "App\\View\\Components\\{$className}";
+        if(class_exists($className)) {
+            $reflection = (new \ReflectionClass($className))->getConstructor();
+            $parameters = [];
+            foreach ($reflection->getParameters() as $param) {
+                $parameters[] = $props[$param->name] ?? $param->getDefaultValue();
+            }
+            $component = new $className(...$parameters);
+            $component->withAttributes($attributes);
+            return $component->render()->with($component->data());
         }
-        $component = new $className(...$parameters);
-        $component->withAttributes($attributes);
-        return $component->render()->with($component->data());
-    }
 
-    $props['attributes'] = new \Illuminate\View\ComponentAttributeBag($attributes);
-    return View::make("components.{$name}", $props);
+        $props['attributes'] = new ComponentAttributeBag($attributes);
+        return View::make("components.{$name}", $props);
+    }
 }
